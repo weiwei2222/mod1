a2c3642a1504ab502d935ab2a0beaa7a3ab60625
Weiwei <linweiwei2222@163.com>
2023-10-10 10:05:14 -0400

    change backgroundcolor

--- JS_practise/guess_number/script.js
+++ JS_practise/guess_number/script.js
@@ -8,17 +8,26 @@ let score = 20;
 document.querySelector(".check").addEventListener("click",function(){
     let guessnumber = document.querySelector(".guess").value;
     console.log(guessnumber, typeof guessnumber);
-    if(guessnumber == ""){
-        document.querySelector(".message").textContent = "Please in a number."
-    }else if (guessnumber === secretnumber){
-        document.querySelector(".message").textContent = "Corret number."
-    }else if (guessnumber > secretnumber){
-        document.querySelector(".message").textContent = "Too hight."
-        score--;
+    if (score > 0){
+        if(guessnumber == ""){
+            document.querySelector(".message").textContent = "Please in a number.";
+        }else if (guessnumber == secretnumber){
+            document.querySelector(".message").textContent = "Corret number.";
+            document.querySelector("body").style.backgroundColor = "#60b347";
+            document.querySelector(".number").textContent = secretnumber;
+        }else if (guessnumber > secretnumber){
+            document.querySelector(".message").textContent = "Too hight.";
+            score--;
+            document.querySelector(".score").textContent = score;
+        }else if (guessnumber < secretnumber){
+            document.querySelector(".message").textContent = "Too low.";
+            score--;
+            document.querySelector(".score").textContent = score;
+        }
+    }else{
+        score = 0;
         document.querySelector(".score").textContent = score;
-    }else if (guessnumber < secretnumber){
-        document.querySelector(".message").textContent = "Too low."
-        score--;
-        document.querySelector(".score").textContent = score;
+        document.querySelector(".message").textContent = "You lost."
     }
+    
 })
