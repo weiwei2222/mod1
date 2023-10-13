d0e2088399f3337bdd53c7e2a35d8b3a643231df
Weiwei <linweiwei2222@163.com>
2023-10-12 22:31:37 -0400

    practise

--- JS_practise/pig_game/script.js
+++ JS_practise/pig_game/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const btnRoll = document.querySelector(".btn--roll")
+const dicPicture = document.querySelector(".dice");
+dicPicture.classList.add("hidden")
+
+const player0_currentscore = document.getElementById("current--0");
+const player1_currentscore = document.getElementById("current--1");
+const btnhold = document.querySelector(".btn--hold")
+
+
+let player0_score_value = 0;
+let player1_score_value = 0;
+
+const player0_score = document.getElementById("score--0");
+const player1_score = document.getElementById("score--1");
+player0_score.textContent = player0_score_value;
+player1_score.textContent = player1_score_value;
+
+let currentscore = 0;
+btnRoll.addEventListener("click",function(){
+    let dicnumber = Math.trunc(Math.random() * 6 +1);
+    console.log(dicnumber)
+    dicPicture.classList.remove("hidden")
+    dicPicture.src = `dice-${dicnumber}.png`;
+    if (dicnumber !== 1){
+        currentscore += dicnumber;
+        player0_currentscore.textContent = currentscore;
+        console.log(currentscore)
+    }
+})
+
+btnhold.addEventListener("click",function(){
+    player0_score_value += currentscore;
+    player0_score.textContent = player0_score_value;
+    currentscore = 0;
+
+})
--- review/ifelse.js
+++ review/ifelse.js
@@ -1,15 +1,15 @@
-// if(' ') {
-//     console.log('hello')
-//     if(0) {
-//         console.log('goodbye')
-//     } else {
-//         console.log('hello')
-//     }
-// } else if(3 === '3') {
-//     console.log('the answer')
-// } else {
-//     console.log('i had a great lunch')
-// }
+if("0") {
+    console.log('hello')
+    if(0) {
+        console.log('goodbye')
+    } else {
+        console.log('hello!!')
+    }
+} else if(3 === '3') {
+    console.log('the answer')
+} else {
+    console.log('i had a great lunch')
+}
 
 /*
 1. if([]) => true
@@ -17,39 +17,39 @@
 3. if(0) => false
 4. log 'hello'
 */
-let teo = 'teo'
-// JavaScript recognizes empty space string ' ' and converts it to 0
-// console.log(' ' == 0) // true
+// let teo = 'teo'
+// // JavaScript recognizes empty space string ' ' and converts it to 0
+// // console.log(' ' == 0) // true
 
-//&& => AND
-//true && true
-if(3 === 3 && 4 == '4') {
-    console.log('Now everybody asks me why')
-    // true
-    if(4 === 4) {
-        console.log("I'm smilin' out from ear to ear")
-        // true
-        if(3 == '3') {
-            console.log('But I know')
-        }
-    } else {
-        console.log('Come on, baby its you!')
-    }
-    console.log("Nothin's perfect, but it's worth it")
-    if(3 * 3) console.log("After fightin' through my tears")
-    if(undefined){
-        console.log('And finally, you put me first')
-    } else {
-        console.log('first')
-    }
-} else {
-    console.log('Baby, its you!')
-}
-if(teo) {
-    console.log('Finally')
-} else {
-    console.log('Youre the one for me')
-}
+// //&& => AND
+// //true && true
+// if(3 === 3 && 4 == '4') {
+//     console.log('Now everybody asks me why')
+//     // true
+//     if(4 === 4) {
+//         console.log("I'm smilin' out from ear to ear")
+//         // true
+//         if(3 == '3') {
+//             console.log('But I know')
+//         }
+//     } else {
+//         console.log('Come on, baby its you!')
+//     }
+//     console.log("Nothin's perfect, but it's worth it")
+//     if(3 * 3) console.log("After fightin' through my tears")
+//     if(undefined){
+//         console.log('And finally, you put me first')
+//     } else {
+//         console.log('first')
+//     }
+// } else {
+//     console.log('Baby, its you!')
+// }
+// if(teo) {
+//     console.log('Finally')
+// } else {
+//     console.log('Youre the one for me')
+// }
 
 // Now everybody asks me why
 // I'm smilin' out from ear to ear
