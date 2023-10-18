224866e6b03f8956cb1c6f21f6a4c84e354b3749
Weiwei <linweiwei2222@163.com>
2023-10-17 20:56:13 -0400

    practice

---
+++ review/for.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+
+for (let i = 0; i < 5; i ++){
+    //console.log(i)
+    for (let x = 0; x < i+1; x ++){
+        //console.log("#");
+        document.write("#");
+    }
+    document.write("<br>");
+}
