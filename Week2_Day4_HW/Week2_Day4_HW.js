79192f7d882ccb3c25da1a110fdd8ec8364379bc
Weiwei <linweiwei2222@163.com>
2023-10-19 17:39:28 -0400

    homework

--- Week2_Day4_HW/Week2_Day4_HW.js
+++ Week2_Day4_HW/Week2_Day4_HW.js
@@ -1,16 +1,41 @@
 'use strict';
 
 
+//Challenge: While Loops - Conditional Expression
+let addThis = 0;
+let sum = 0;
+while (addThis < 10){
+    sum = sum + addThis;
+    addThis ++;
+}
+console.log(sum);
+
+
+
+
+//Challenge: Control Flow - if statements
+const num = 40;
+let final;
+if (num > 100){
+    final = null;
+}else{
+    final = num *2;
+}
+console.log(num)
+
+
+
+
+
+//Challenge: Fizzbuzz/Fizzbuzz Solution
 //Use a loop to iterate through the numbers 1 through 16. 
 //Push each number into fb, but push the string "fizz" in place of numbers divisible by 3, 
 //"buzz" in place of numbers divisible by 5, 
 //and "fizzbuzz" in place of numbers divisible by both 3 and 5.
 //Log fb to the console to see the output.
 //Hint: Check out the remainder/modulo operator: %.
-
 // should log: [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16]
 const fb = [];
-
 for (let i = 1; i <= 16; i ++){
     fb.push(i)
     if (i%3==0 && i%5==0){
