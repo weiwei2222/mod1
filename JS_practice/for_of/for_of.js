dbc105b50deacb89ccb09ea8bfc4f902264654df
Weiwei <linweiwei2222@163.com>
2023-10-21 18:48:38 -0400

    practice

--- JS_practice/for_of/for_of.js
+++ JS_practice/for_of/for_of.js
@@ -70,6 +70,19 @@ const game = {
       team2: 6.5,
     },
   };
-
-  for (const [i, player] of game.scored.entries())
+// 1.
+for (const [i, player] of game.scored.entries())
   console.log(`Goal ${i + 1}: ${player}`);
+
+// 2.
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average);
+
+// 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
