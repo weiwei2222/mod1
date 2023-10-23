60e119958580c2bb134e836f41633fe74aa8c93a
Weiwei <linweiwei2222@163.com>
2023-10-22 22:24:29 -0400

    practice

--- JS_practice/slide/slide.js
+++ JS_practice/slide/slide.js
@@ -1,11 +1,10 @@
 'use strict';
 let slideIndex = 0;
+const slides = document.getElementsByClassName("mySlides");
+const dots = document.getElementsByClassName("dot");
+
 showSlides();
-
 function showSlides() {
-    const slides = document.getElementsByClassName("mySlides");
-    const dots = document.getElementsByClassName("dot");
-
     for (let i = 0; i < slides.length; i++) {
        slides[i].style.display = "none";  
     };
@@ -16,7 +15,18 @@ function showSlides() {
     for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     };
-    slides[slideIndex-1].style.display = "block";  
+    slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 3000); 
+    setTimeout(showSlides, 3000);
 }
+
+for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener('click', function(){
+            slides[i].style.display = "block";
+            slides[slideIndex-1].style.display = "none";
+            slides[slideIndex+1].style.display = "none";
+            dots[i].className += " active";
+        
+        
+    });
+ }
