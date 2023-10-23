cbc7ea7506b27afd0d5d54566bdd0ca6d8bc18fb
Weiwei <linweiwei2222@163.com>
2023-10-23 10:34:14 -0400

    practice

---
+++ Space_Battle/Space_Battle.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class Example {
+	constructor(name){
+		this.name = name
+		this.method = this.method.bind(this)
+	}
+
+	methodFromExample(){
+	 return this.name
+	}
+}
+
+const btnEl = document.querySelector('button')
+
+btnEl.addEventListener('click', methodFromExample)
