494b5701e7acaab7c63a74d9c7a90174ebd66299
Weiwei <linweiwei2222@163.com>
2023-10-22 20:29:31 -0400

    practice

--- JS_practice/slide/slide.js
+++ JS_practice/slide/slide.js
@@ -3,20 +3,20 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    let i = 0;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
+    const slides = document.getElementsByClassName("mySlides");
+    const dots = document.getElementsByClassName("dot");
+
+    for (let i = 0; i < slides.length; i++) {
        slides[i].style.display = "none";  
     };
     slideIndex++;
     if (slideIndex > slides.length) {
         slideIndex = 1
     }; 
-    for (i = 0; i < dots.length; i++) {
+    for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     };
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); 
+    setTimeout(showSlides, 3000); 
 }
