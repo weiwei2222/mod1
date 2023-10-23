aedb83aa0181b7bfc9935ae5c033b56999fd59b6
Weiwei <linweiwei2222@163.com>
2023-10-23 13:57:20 -0400

    study

--- dom/js/dom.js
+++ dom/js/dom.js
@@ -2,7 +2,16 @@
 
 let titleEl = document.getElementById('title');
 console.log(titleEl);
+console.dir(titleEl);
 
-
-let textEl = document.querySelector('h3');
+let textEl = document.querySelector('.text');
 console.log(textEl);
+
+let coolEl = document.querySelector('.cool');
+console.log(coolEl);
+
+
+textEl.innerHTML = 'This is new text. Today is <strong>Monday</strong>!!!!I changed this text.'
+coolEl.textContent = 'This is new. haha, I am happy!!I changed this p.'
+textEl.style.textAlign = 'center';
+
