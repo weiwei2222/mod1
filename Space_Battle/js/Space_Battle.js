26995f2e23c244e55dcbeff4bbd1bf9ff1065202
Weiwei <linweiwei2222@163.com>
2023-10-24 17:17:54 -0400

    practice

--- Space_Battle/js/Space_Battle.js
+++ Space_Battle/js/Space_Battle.js
@@ -3,7 +3,8 @@
 const attackbtnEl = document.querySelector('.attackbtn')
 const retreatbtnEl = document.querySelector('.retreatbtn')
 const showEl = document.querySelector('.show');
-console.log(showEl);
+const show1El = document.querySelector('.show1');
+console.log(show1El);
 
 
 class Ship{
@@ -16,17 +17,22 @@ class Ship{
 	attackAlien(alienShip){
 		if (Math.random() < this.accuracy){
 			alienShip[0].hull = alienShip[0].hull - this.firepower;
-			showEl.textContent = `You are attacking alien! Alien hull is ${alienShip[0].hull}.`;
-			//console.log(alienships);
+			showEl.textContent = `You are attacking alien! Alien hull is ${alienShip[0].hull} now.`;
 			if (alienShip[0].hull <= 0){
+				show1El.textContent = 'This Alien ship is destroyed.';
 				alienships.splice(alienships[0],1,);
 				console.log(alienships);
+			}else{
+				// setTimeout(alienships[0].attackShip(myship), 2000);
+				alienships[0].attackShip(myship);
 			}
 		}else{
 			showEl.textContent = 'You missed!';
 		}
 	}
-
+	retreat(){
+		showEl.textContent = 'You retreat!';
+	}
 }
 
 class Alienship{
@@ -38,13 +44,7 @@ class Alienship{
 	attackShip(myship){
 		if (Math.random() > this.accuracy){
 			myship.hull = myship.hull - this.firepower;
-			console.log(`You got hit by alien!Your ship hull is ${myship.hull}.`);
-			showEl.textContent = `You got hit by alien!Your ship hull is ${myship.hull}.`;
-			if (this.hull <= 0){
-				console.log('Alien ship is destroyed.');
-			}
-		}else{
-			console.log('You are attacking alien!');
+			show1El.textContent = `You got hit by alien!Your ship hull is ${myship.hull}.`;
 		}
 	}
 }
@@ -59,7 +59,9 @@ for (let i = 0; i < 6; i ++){
 
 attackbtnEl.addEventListener("click",function(){
 	myship.attackAlien(alienships);
-
 	
 });
 
+retreatbtnEl.addEventListener('click',function(){
+	myship.retreat();
+})
