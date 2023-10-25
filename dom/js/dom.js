915db609c14b670c762ce489abd4bd5aa17ac32b
Weiwei <linweiwei2222@163.com>
2023-10-25 10:34:06 -0400

    practice

--- dom/js/dom.js
+++ dom/js/dom.js
@@ -14,4 +14,6 @@ console.log(coolEl);
 textEl.innerHTML = 'This is new text. Today is <strong>Monday</strong>!!!!I changed this text.'
 coolEl.textContent = 'This is new. haha, I am happy!!I changed this p.'
 textEl.style.textAlign = 'center';
+textEl.style.color = 'red';
+coolEl.style.backgroundColor = '#666';
 
