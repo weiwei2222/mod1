6ed1293a89635b323efc419b461628f23532ba63
Weiwei <linweiwei2222@163.com>
2023-10-25 15:41:39 -0400

    practice

--- Week5_Day1_HW/js/script.js
+++ Week5_Day1_HW/js/script.js
@@ -1,5 +1,28 @@
+'use strict';
+
+// Menu data structure
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
 
 
 const mainEl = document.querySelector('main');
 
-console.log(mainEl)
+mainEl.style.backgroundColor = 'var(--main-bg)';
+
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+
+mainEl.classList.add('flex-ctr');
+
+const topMenuEl = document.getElementById('top-menu');
+
+topMenuEl.style.width = '100%';
+
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+
+topMenuEl.classList.add('flex-around');
+console.log(topMenuEl)
+
