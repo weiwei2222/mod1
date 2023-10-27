e73046ec2dcd657e7b020ea2147bce606c96114b
Weiwei <linweiwei2222@163.com>
2023-10-27 13:23:25 -0400

    practice

--- Space_Battle/js/Space_Battle.js
+++ Space_Battle/js/Space_Battle.js
@@ -8,6 +8,11 @@ const myhull = document.getElementById('current--0');
 const alienhull = document.getElementById('current--1');
 let playing = true;
 let current = 0; 
+const imgList = document.querySelectorAll('.img')
+for (let current = 0; current < imgList.length; current ++){
+	console.log(imgList[current])
+}
+
 
 class Ship{
 	constructor(name){
@@ -17,14 +22,12 @@ class Ship{
 		this.accuracy = 0.7;
 	}
 	attackAlien(alien){
-		console.log(alien.hull)
 		if (Math.random() < this.accuracy){
 			alien.hull -= this.firepower;
 			showEl.textContent = `You are attacking alien!`;
 			alienhull.textContent = `${alien.hull}`;
 		}else{
 			showEl.textContent = 'You missed!';
-			alien.attackShip(this);
 		}
 	}
 	retreat(){
@@ -45,7 +48,7 @@ class Alienship{
 	attackShip(myship){
 		if (Math.random() < this.accuracy){
 			myship.hull -= this.firepower;
-			show1El.textContent = `You got hit by alien!`;
+			show1El.textContent = `You got hit by alien! Your ship got some damage`;
 			myhull.textContent = `${myship.hull}`
 		}else{
 			show1El.textContent = `You got hit by alien!But alien missed.`;
@@ -57,7 +60,6 @@ class Alienship{
 }
 const myship = new Ship('USS Assembly')
 const alienshipsArray = [];
-const imgList = [];
 for (let i = 0; i < 6; i ++){
 	const alien = new Alienship();
 	alienshipsArray.push(alien);
@@ -68,16 +70,19 @@ alienhull.textContent = alienshipsArray[0].hull;
 const game = function(){
 		if (playing){
 			myship.attackAlien(alienshipsArray[current]);
-			current ++ ;
-			console.log(alienshipsArray[current]);
 			if (alienshipsArray[current].hull <= 0){
-				alienshipsArray[current].aliendie();
+				show1El.textContent = `One alien ship destroyed.`;
+				imgList[current].classList.add('hidden');
+				console.log(current);
+				current ++;
 			}else if (alienshipsArray[current].hull > 0){
+				show1El.textContent = `One alien ship survives.`;
 				alienshipsArray[current].attackShip(myship);
 			}
-			if (current >= 5 || myship.hull <= 0){
+			if (current > 5 || myship.hull <= 0){
 				playing = false;
 				showEl.textContent = 'You win!';
+				show1El.textContent = 'All alien ships are destroyed.'
 			}
 		}
 }
