7eee6264948ed3cb14c823983eef2bd23e8204f0
Weiwei <linweiwei2222@163.com>
2023-10-28 16:12:39 -0400

    practice

--- JS_practice/bmi/js/bmi.js
+++
@@ -1,7 +0,0 @@
-
-function bmiCalculator() {
-    const myweight = document.getElementById("userweight").value;
-    const myheight = document.getElementById("userheight").value;
-    const userbmi = myweight / myheight ** 2;
-    document.getElementById("result").innerHTML = `You BMI is ${userbmi}`;
-}
