9a01421e6e24cb00247900fcf8d7f6b8bad17c43
Weiwei <linweiwei2222@163.com>
2023-10-30 14:46:04 -0400

    add flowchart wire frames

--- MemoryGame/js/script.js
+++ MemoryGame/js/script.js
@@ -2,26 +2,51 @@
 
 const cards = document.querySelectorAll('.memory-card');
 const movesEl = document.getElementById('moves');
-console.log(movesEl)
+const timer = document.getElementById('timer');
+const reset = document.getElementById('reset');
 
 let checkFlip = false;
 let firstCard, secondCard;
+let lockCard = false;
 let moves = 0;
+let seconds = 0;
+let minutes = 0;
+let playing = true;
+
+function resetGame(){
+  cards.forEach(card => card.classList.remove('flip'));
+  reset2Cords()
+  moves = 0;
+  movesEl.textContent = moves;
+  seconds = 0;
+  minutes = 0;
+  (function shuffle(){
+    cards.forEach(card => {
+      let randomNum = Math.floor(Math.random() *16);
+      card.style.order = randomNum;
+    } )
+  })()
+}
+
+function reset2Cords(){
+  [checkFlip, lockCard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+  
+}
 
 function flipCard() {
+  if (lockCard) return;
+  if (this === firstCard) return;
   this.classList.add('flip');
   if (!checkFlip){
     checkFlip = true;
     firstCard = this;
-    console.log(firstCard);
-    return
+    return;
   }
   secondCard = this;
   checkFlip = false;
-  console.log(secondCard);
   moves ++;
   movesEl.textContent = moves;
-  console.log(movesEl);
   checkMatch()
 }
 
@@ -39,25 +64,38 @@ function checkMatch(){
 function disableCards() {
    firstCard.removeEventListener('click', flipCard);
    secondCard.removeEventListener('click', flipCard);
+   reset2Cords();
 }
 
 function unflipCards() {
-   setTimeout(() => {
+  lockCard = true;
+  setTimeout(() => {
      firstCard.classList.remove('flip');
      secondCard.classList.remove('flip');
-   }, 1500);
+     reset2Cords();
+  }, 500);
 }
 
 (function shuffle(){
   cards.forEach(card => {
     let randomNum = Math.floor(Math.random() *16);
     card.style.order = randomNum;
-    console.log(randomNum)
   } )
 })()
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+function updateTimer(){
+  seconds ++;
+  if (seconds === 60){
+    seconds = 0;
+    minutes ++;
+  }
+  timer.textContent = `${minutes} : ${seconds}`;
+}
+// setInterval(updateTimer, 1000)
 
+cards.forEach(card => card.addEventListener('click', flipCard));
 // for (let i = 0; i <cards.length; i ++){
 //   cards[i].addEventListener('click', flipCard);
 // }
+
+reset.addEventListener('click',resetGame)
