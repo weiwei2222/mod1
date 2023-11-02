06cfeea91188746000dbfefcbc0b9386974d62a8
Weiwei <linweiwei2222@163.com>
2023-11-02 09:26:01 -0400

    fix timer

--- MemoryGame/js/script.js
+++ MemoryGame/js/script.js
@@ -12,9 +12,9 @@ let seconds = 0;
 let minutes = 0;
 let timer = false;
 let match = 0;
-
+// when html page onload, call shuffle funtion. shuffle all cards.
 document.body.onload = shuffle();
-
+// reset the game
 function resetGame(){
   cards.forEach(card => card.classList.remove('flip'));
   cards.forEach(card => card.addEventListener('click', flipCard));
@@ -27,16 +27,20 @@ function resetGame(){
   minutes = 0;
   shuffle();
 }
-
+// reset 2 cords
 function reset2Cords(){
   [checkFlip, lockCard] = [false, false];
   [firstCard, secondCard] = [null, null];
 }
-
+// flipcard when user click a card. and the moves and timer will start.
 function flipCard() {
   if (lockCard) return;
   if (this === firstCard) return;
   this.classList.add('flip');
+  moves ++;
+  if (moves == 1){
+    timer = setInterval(updateTimer, 1000);
+  }
   if (!checkFlip){
     checkFlip = true;
     firstCard = this;
@@ -44,14 +48,11 @@ function flipCard() {
   }
   secondCard = this;
   checkFlip = false;
-  moves ++;
   movesEl.textContent = moves;
-  if (moves == 1){
-    timer = setInterval(updateTimer, 1000);
-  }
   checkMatch();
 }
-
+// check 2 cards match or not, if it is match, 2 cards turn to disablecards, if it isn't match, turn to unflipcards
+// and also check all card wether match. if it is match 8 times,the moves and timer will stop.
 function checkMatch(){
   if (firstCard.dataset.animal === secondCard.dataset.animal){
     match ++;
@@ -63,13 +64,13 @@ function checkMatch(){
   }else{
     unflipCards();}
 }
-
+// disable 2 cards
 function disableCards() {
    firstCard.removeEventListener('click', flipCard);
    secondCard.removeEventListener('click', flipCard);
    reset2Cords();
 }
-
+// unflip 2 cards
 function unflipCards() {
   lockCard = true;
   setTimeout(() => {
@@ -78,14 +79,14 @@ function unflipCards() {
      reset2Cords();
   }, 1000);
 }
-
+// shuffle all cards. and change all cards order.
 function shuffle(){
   cards.forEach(card => {
     let randomNum = Math.floor(Math.random() *16);
     card.style.order = randomNum;
   } );
 }
-
+// this is game timer.
 function updateTimer(){
   seconds ++;
   if (seconds === 60){
